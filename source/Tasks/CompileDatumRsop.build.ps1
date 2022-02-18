914770c1ea11bac59a75bc9c2e3c173fd5b79a94
Raimund Andree <r.andree@live.com>
2022-02-18 11:13:35 +0100

    Generating RSOP files with source and clearing cache

--- source/Tasks/CompileDatumRsop.build.ps1
+++ source/Tasks/CompileDatumRsop.build.ps1
@@ -33,26 +33,42 @@ param
 
     [Parameter()]
     [string]
+    $RsopWithSourceFolder = (property RsopFolderWithSource 'RsopWithSourceFolder'),
+
+    [Parameter()]
+    [string]
     $ModuleVersion = (property ModuleVersion '')
 )
 
 task CompileDatumRsop {
+
+    Clear-DatumCache #otherwise this task will not generate new RSOP data
+
     # Get the vales for task variables, see https://github.com/gaelcolas/Sampler#task-variables.
     . Set-SamplerTaskVariable -AsNewBuild
 
     $DatumConfigDataDirectory = Get-SamplerAbsolutePath -Path $DatumConfigDataDirectory -RelativeTo $ProjectPath
     $RsopFolder = Get-SamplerAbsolutePath -Path $RsopFolder -RelativeTo $OutputDirectory
+    $RsopWithSourceFolder = Get-SamplerAbsolutePath -Path $RsopWithSourceFolder -RelativeTo $OutputDirectory
 
     if (-not (Test-Path -Path $RsopFolder))
     {
         $null = New-Item -ItemType Directory -Path $RsopFolder -Force
     }
+    if (-not (Test-Path -Path $RsopWithSourceFolder))
+    {
+        $null = New-Item -ItemType Directory -Path $RsopWithSourceFolder -Force
+    }
 
     $rsopOutputPathVersion = Join-Path -Path $RsopFolder -ChildPath $ModuleVersion
-
     if (-not (Test-Path -Path $rsopOutputPathVersion))
     {
         $null = New-Item -ItemType Directory -Path $rsopOutputPathVersion -Force
+    }
+    $rsopWithSourceOutputPathVersion = Join-Path -Path $RsopWithSourceFolder -ChildPath $ModuleVersion
+    if (-not (Test-Path -Path $rsopWithSourceOutputPathVersion))
+    {
+        $null = New-Item -ItemType Directory -Path $rsopWithSourceOutputPathVersion -Force
     }
 
     if ($configurationData.AllNodes)
@@ -60,8 +76,11 @@ task CompileDatumRsop {
         Write-Build Green "Generating RSOP output for $($configurationData.AllNodes.Count) nodes."
         $configurationData.AllNodes.Where({$_['Name'] -ne '*'}) | ForEach-Object -Process {
             Write-Build Green "`tBuilding RSOP for $($_['NodeName'])..."
-            $nodeRSOP = Get-DatumRsop -Datum $datum -AllNodes ([ordered]@{ } + $_)
-            $nodeRSOP | ConvertTo-Json -Depth 40 | ConvertFrom-Json | Convertto-Yaml -OutFile (Join-Path -Path $rsopOutputPathVersion -ChildPath "$($_['NodeName']).yml") -Force
+            $nodeRsop = Get-DatumRsop -Datum $datum -AllNodes ([ordered]@{ } + $_) -RemoveSource
+            $nodeRsop | ConvertTo-Json -Depth 40 | ConvertFrom-Json | Convertto-Yaml -OutFile (Join-Path -Path $rsopOutputPathVersion -ChildPath "$($_.Name).yml") -Force
+
+            $nodeRsopWithSource = Get-DatumRsop -Datum $datum -AllNodes ([ordered]@{ } + $_) -IncludeSource
+            $nodeRsopWithSource | ConvertTo-Json -Depth 40 | ConvertFrom-Json | Convertto-Yaml -OutFile (Join-Path -Path $rsopWithSourceOutputPathVersion -ChildPath "$($_.Name).yml") -Force
         }
     }
     else
