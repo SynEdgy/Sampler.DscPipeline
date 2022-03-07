010065920c69baa90fb4114c8b0cda6cdcaccb2e
Raimund Andree <r.andree@live.com>
2022-03-07 15:29:50 +0100

    Fixed property name

--- source/Tasks/NewMofChecksums.build.ps1
+++ source/Tasks/NewMofChecksums.build.ps1
@@ -28,7 +28,7 @@ task NewMofChecksums {
     $mofs = Get-ChildItem -Path $MofOutputFolder -Recurse -ErrorAction SilentlyContinue
     foreach ($mof in $mofs)
     {
-        if ($mof.BaseName -in $global:configurationData.AllNodes.NodeName)
+        if ($mof.BaseName -in $global:configurationData.AllNodes.Name)
         {
             New-DscChecksum -Path $mof.FullName -Verbose:$false -Force
         }
