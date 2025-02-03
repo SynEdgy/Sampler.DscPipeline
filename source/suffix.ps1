75495c84de803ce8eef0ebe08e6030fdfb211f20
Raimund Andree <r.andree@live.com>
2025-02-03 08:55:47 +0100

    Fix case sensitivity in path for task file retrieval in 'suffix.ps1' (from PR #33)

--- source/suffix.ps1
+++ source/suffix.ps1
@@ -1,5 +1,5 @@
 # Inspired from https://github.com/nightroman/Invoke-Build/blob/64f3434e1daa806814852049771f4b7d3ec4d3a3/Tasks/Import/README.md#example-2-import-from-a-module-with-tasks
-Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'tasks\*') -Include '*.build.*' |
+Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Tasks\*') -Include '*.build.*' |
     ForEach-Object -Process {
         $ModuleName = ([System.IO.FileInfo] $MyInvocation.MyCommand.Name).BaseName
         $taskFileAliasName = "$($_.BaseName).$ModuleName.ib.tasks"
